f0ff2296aa8fce928383d9ea1a767ef7884300a5
racec9999 <60860385+racec9999@users.noreply.github.com>
2020-03-11 20:29:09 -0600

    Getting twitter data
    You need to install tweepy to run this file

---
+++ twitter_streamer.py
@@ -0,0 +1,27 @@
+from tweepy.streaming import StreamListener
+from tweepy import OAuthHandler
+from tweepy import Stream 
+
+import twitter_credentials
+
+
+class StdOutListener(StreamListener):#inherit from stream listener
+    
+    def on_data(self,data): #listening for tweets
+        print(data)
+        return True
+    
+    def on_error(self,status):
+        print(status)
+        
+        
+        
+if __name__ == "__main__":
+    
+    listener = StdOutListener()
+    auth = OAuthHandler(twitter_credentials.CONSUMER_KEY,twitter_credentials.CONSUMER_KEY_SECRET)
+    auth.set_access_token(twitter_credentials.ACCESS_TOKEN,twitter_credentials.ACCESS_TOKEN_SECRET)
+    
+    stream = Stream(auth,listener)
+    
+    stream.filter(track=['#DonaldTrump'])
