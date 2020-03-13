5cec84975b75e17c1691a25803e60650c3bdeb59
racec9999 <60860385+racec9999@users.noreply.github.com>
2020-03-13 13:09:06 -0600

    Update twitter_streamer.py

--- twitter_streamer.py
+++ twitter_streamer.py
@@ -5,6 +5,12 @@ from tweepy import Stream
 import twitter_credentials
 
 
+from forex_python.converter import CurrencyRates
+
+
+
+
+
 class StdOutListener(StreamListener):#inherit from stream listener
     
     def on_data(self,data): #listening for tweets
@@ -22,6 +28,12 @@ if __name__ == "__main__":
     auth = OAuthHandler(twitter_credentials.CONSUMER_KEY,twitter_credentials.CONSUMER_KEY_SECRET)
     auth.set_access_token(twitter_credentials.ACCESS_TOKEN,twitter_credentials.ACCESS_TOKEN_SECRET)
     
-    stream = Stream(auth,listener)
+    arr = []
     
-    stream.filter(track=['Donald Trump'])
+    stream = Stream(auth,listener)
+    c = CurrencyRates()
+
+    print(c.get_rate('USD', 'MXN'))
+    
+    stream.filter(track=['donald trump'])
+    
