9d24936929c286f92ba3f620f2ad55a668ebe54b
luasikirf <60940567+luasikirf@users.noreply.github.com>
2020-05-02 21:23:27 -0500

    Add files via upload
     the file "db.json" must be on "/home/user_name/name_of_proyect/config" to avoid problems whit the scrip that insert data on the database.

--- collecting_data.py
+++ collecting_data.py
@@ -11,13 +11,18 @@ from datetime import datetime
 import json
 from datetime import timedelta
 
+
+
 class StdOutListener(StreamListener):#inherit from stream listener
+
     def on_data(self,data): #listening for tweets
         print(data)
         return True
 
     def on_error(self,status):
         print(status)
+
+
 
 if __name__ == "__main__":
 
@@ -26,23 +31,33 @@ if __name__ == "__main__":
     auth = OAuthHandler(twitter_credentials.CONSUMER_KEY,twitter_credentials.CONSUMER_KEY_SECRET)
     auth.set_access_token(twitter_credentials.ACCESS_TOKEN,twitter_credentials.ACCESS_TOKEN_SECRET)
     api = tweepy.API(auth,wait_on_rate_limit = True)
+
     arr = []
+
+
     stream = Stream(auth,listener)
-    # automate tweet date
     today = datetime.today().strftime('%Y-%m-%d')
-    tomorrow = datetime.strftime(datetime.now() + timedelta(1),'%Y-%m-%d')
+    yesterday = datetime.strftime(datetime.now() - timedelta(1),'%Y-%m-%d')
+
+    
     tweets = 0
-    # Selection hashtag and day 
-    Tdate = '#DonaldTrump since:'+today+' until:'+tomorrow
+    #'#DonaldTrump since:2020-04-27 until:2020-04-28'
+    Tdate = '#DonaldTrump since:'+yesterday+' until:'+today
     for tweet in tweepy.Cursor(api.search, q=Tdate, count=100).items(MAX_TWEETS):
         tweets+=1
+    
     c = CurrencyRates()   
+    dolar_price = c.get_rate('USD', 'MXN')
+    print("today", today)
+    print("Dolar price: "+ str(dolar_price))
+    print("Total tweets: " + str(tweets))
+    
     save_file = {
         'date': today,
         'tweet': tweets,
-        'dollar': dolar_price
+        'dolar': dolar_price
         }
     
-    file_name = "tweet_collector"+today+".json"
-    with open(file_name,'w') as json_file:
+    
+    with open('tweet_collector.json','w') as json_file:
         json.dump(save_file,json_file)
